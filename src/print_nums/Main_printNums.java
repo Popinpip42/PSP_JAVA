1f8ca3a42f1212f0f8e098f278862dabd380a1b0
Popinpip42 <luisxl10@gmail.com>
2023-09-17 23:14:47 +0200

    "Ejer 2 corregido"

--- src/print_nums/Main_printNums.java
+++ src/print_nums/Main_printNums.java
@@ -4,14 +4,13 @@ public class Main_printNums {
 	static final Object lock = new Object();
 	static int num = 1;
 	public static void main(String[] args) {
-		int size = 5;
-		
-		while (size-- > 0)
+		int n_threads = 2;
+		int	i = 0;
+		while (i < n_threads)
 		{
-			Thread nums_t1 = new Print_nums_T();
-			Thread nums_t2 = new Print_nums_T();
-			nums_t1.start();
-			nums_t2.start();
+			Thread nums_t = new Print_nums_T();
+			nums_t.start();
+			i++;
 		}
 	}
 }
--- src/print_nums/Print_nums_T.java
+++ src/print_nums/Print_nums_T.java
@@ -1,17 +1,23 @@
 package print_nums;
 
-public class Print_nums_T extends Thread{	
-	
+public class Print_nums_T extends Thread {
+
 	@Override
-	public void run()
-	{
-		synchronized (Main_printNums.lock) {
-			if (Main_printNums.num % 2 == 0) {
-				System.out.println("Even: " + Main_printNums.num );
-				Main_printNums.num ++;
-			}else if (Main_printNums.num  % 2 != 0) {
-				System.out.println("Odd : " + Main_printNums.num );
-				Main_printNums.num ++;
+	public void run() {
+		while (true) {
+			synchronized (Main_printNums.lock) {
+				if (Main_printNums.num <= 10) 
+				{
+					if (Main_printNums.num % 2 == 0) {
+						System.out.println("Even: " + Main_printNums.num + ", ");
+						Main_printNums.num++;
+					} else {
+						System.out.println("Odd : " + Main_printNums.num + ", ");
+						Main_printNums.num++;
+					}
+				}
+				else
+					break;
 			}
 		}
 	}
